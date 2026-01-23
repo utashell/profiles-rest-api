b8f6f518f4f4e21fdcfa1782d559b07ef3c2d328
Sadaf Jalilzadeh <jalilzadesadaf@gmail.com>
2026-01-23 18:04:55 +0330

    Added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.conf import settings
 
 # Create your models here.
 
@@ -54,3 +55,18 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+    
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    status_text = models.CharField(max_length=225)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return the model as string"""
+        return self.status_text    
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from . import models
@@ -114,4 +115,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES    
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES  
+    
+    
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+    
+    def perform_create(self, serializer):
+        """Set the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)      
